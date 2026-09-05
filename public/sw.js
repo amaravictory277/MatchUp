c25a6a01c0b8d5cc9a6576231d8e9eeb3ece7710
amaravictory277 <amaravictory277@gmail.com>
2026-09-04 17:32:35 -0700

    feat: scaffold MatchUp tournament PWA

---
+++ public/sw.js
@@ -0,0 +1,2 @@
+self.addEventListener("install", event => event.waitUntil(caches.open("matchup-v1").then(cache => cache.addAll(["/", "/manifest.webmanifest", "/icon.svg"]))));
+self.addEventListener("fetch", event => event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request))));
