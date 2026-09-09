e5121e587f94e4ea9971d3db0cf18dfd9d6fc5a6
Amara Victory <amaravictory277@gmail.com>
2026-09-09 07:28:52 -0700

    Make Chat a dedicated full-screen experience without global navigation

--- app/leaderboard/page.tsx
+++ app/leaderboard/page.tsx
@@ -1,15 +1,5 @@
-import { BottomNav, TopBar } from "../../components/navigation";
 import { ChatHub } from "../../components/chat/chat-hub";
 
 export default function LeaderboardPage() {
-  return (
-    <>
-      <main className="app-shell pb-28">
-        <TopBar />
-        <ChatHub />
-      </main>
-      <BottomNav />
-    </>
-  );
+  return <main className="min-h-screen bg-[#061120]"><ChatHub /></main>;
 }
-
