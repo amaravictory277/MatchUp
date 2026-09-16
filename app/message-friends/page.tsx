7ad999d8fcf8d2c9f4fc5a98db9aed4ef1005599
Amara Victory <amaravictory277@gmail.com>
2026-09-15 18:56:12 -0700

    Switch messaging route to direct DM implementation

--- app/message-friends/page.tsx
+++
@@ -1,4 +0,0 @@
-import { Suspense } from "react";
-import { MessageFriendsPage } from "../../components/chat/message-friends-page";
-
-export default function MessageFriendsRoute(){return <Suspense fallback={<main className="min-h-screen bg-[#061120]"/>}><MessageFriendsPage/></Suspense>}
